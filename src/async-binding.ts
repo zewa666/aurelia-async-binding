bc50ebbec4c4dac802463ee77a44eb6265adf7ee
Vildan Softic <zewa666@gmail.com>
2017-11-09 19:42:50 +0100

    feat(async): error handlers; pluck for promises
    Promises success cases now support the pluck feature as well;
    Promise and Observable catch/error handlers now can be used either as handlers (if function) or fallback values (no pluck)

--- src/async-binding.ts
+++ src/async-binding.ts
@@ -1,16 +1,17 @@
-import { Observable } from 'rxjs/Observable';
-import { Subscription } from 'rxjs/Subscription';
-import { Binding } from 'aurelia-binding';
+import { Observable } from "rxjs/Observable";
+import { Subscription } from "rxjs/Subscription";
+import { Binding } from "aurelia-binding";
 
 export interface AsyncAureliaBinding extends Binding {
   originalupdateTarget(value: any): void;
   _subscription?: Subscription;
 }
 
-export interface IAsyncBindingBehaviorOptions {
-  property: string;
-  error: (res: any) => void;
+export interface AsyncBindingBehaviorOptions {
+  catch: any;
   completed: () => void;
+  error: any;
+  property: string;
 }
 
 export class asyncBindingBehavior {
@@ -21,20 +22,30 @@ export class asyncBindingBehavior {
       .reduce((prev, curr) => prev[curr], obj);
   }
 
-  bind(binding: AsyncAureliaBinding, _source: string, options: IAsyncBindingBehaviorOptions) {
+  bind(binding: AsyncAureliaBinding, _source: string, options?: AsyncBindingBehaviorOptions) {
     binding.originalupdateTarget = binding.updateTarget || (() => {});
 
     binding.updateTarget = (a) => {
-      if (typeof a.then === 'function') {
-        a.then((d: any) => {
-          binding.originalupdateTarget(d);
-        });
+      if (typeof a.then === "function") {
+        a.then((res: any) => binding.originalupdateTarget(options && options.property ? this.getPropByPath(res, options.property) : res));
+        
+        if (options && options.catch) {
+          a.catch((res: any) => typeof options.catch === "function"
+            ? options.catch(res)
+            : binding.originalupdateTarget(options.catch));
+        }
       } else if (a instanceof Observable) {
+        const error = options
+          ? typeof options.error === "function"
+            ? options.error
+            : () => { binding.originalupdateTarget(options && options.property ? this.getPropByPath(options.error, options.property) : options.error); }
+          : undefined;
+
         binding._subscription = a.subscribe(
           (res) => {
             binding.originalupdateTarget(options && options.property ? this.getPropByPath(res, options.property) : res);
           },
-          options ? options.error : undefined,
+          error,
           options ? options.completed : undefined
         );
       }
@@ -50,7 +61,7 @@ export class asyncBindingBehavior {
     (binding as any).originalupdateTarget = undefined;
 
     if (binding._subscription &&
-      typeof binding._subscription.unsubscribe === 'function') {
+      typeof binding._subscription.unsubscribe === "function") {
       binding._subscription.unsubscribe();
     }
   }
